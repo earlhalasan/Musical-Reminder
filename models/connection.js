d7082e07bdcc1738038de73eafc86737f51561db
earlhalasan <earl.halasan@gmail.com>
2022-06-16 13:02:31 -0400

    Added connectivity to heroku

--- models/connection.js
+++ models/connection.js
@@ -9,7 +9,7 @@ const db = mongoose.connection;
 // Database Connection
 /////////////////////////////////////////////
 // Setup inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.MONGODB_URI;
 const CONFIG = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
